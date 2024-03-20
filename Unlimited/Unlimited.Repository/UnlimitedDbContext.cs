48cc94c598b01e6f21b7464751bc569987c6343c
PepperOnly <david.kuun123@gmail.com>
2024-03-20 16:39:12 +0200

    and ignore

--- Unlimited/Unlimited.Repository/UnlimitedDbContext.cs
+++ Unlimited/Unlimited.Repository/UnlimitedDbContext.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Models;
 
 namespace Unlimited.Repository
 {
   public class UnlimitedDbContext : DbContext
   {
-    public UnlimitedDbContext(DbContextOptions<UnlimitedDbContext> options) : base(options)
+    private readonly DbContextOptions _options;
+
+    public UnlimitedDbContext(DbContextOptions options) : base(options)
     {
+      _options = options;
     }
 
     public DbSet<Card> Cards { get; set; }
